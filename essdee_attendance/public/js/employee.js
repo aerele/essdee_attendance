2f428c0bd56d310ab25888f7189226c8099de530
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-03-06 09:48:25 +0530

    employee js altered with enroll fingerprint and sync now functionality

--- essdee_attendance/public/js/employee.js
+++ essdee_attendance/public/js/employee.js
@@ -1,29 +1,37 @@
 frappe.ui.form.on("Employee", {
-    refresh : (frm) => {
-        if(!frm.doc.__islocal) {
-        frm.add_custom_button(__("Delete Linked User"), function() {
-            frm.trigger('delete_employee');
-        });
-        frm.add_custom_button(__("Enroll User"), function() {
-            frm.trigger('enroll_user');
-        });
-    }
+    enroll_fingerprint: function(frm){
+	let d = new frappe.ui.Dialog({
+		title: __('Enroll Fingerprint'),
+		fields: [
+			{ fieldtype: 'Link', options: 'Essdee Biometric Device', reqd: 1, fieldname: 'select_biometric_device', label: 'Select Biometric Device'},
+		],
+		primary_action_label: __('Enroll'),
+		primary_action: function() {
+			frm.enable_save();
+			frappe.call({
+			method: "essdee_attendance.essdee_attendance.doctype.essdee_attendance_settings.essdee_attendance_settings.enroll_fingerprint",
+			freeze: true, args: {doc: frm.doc, device: d.get_value('select_biometric_device')},
+			callback: function(r) {
+				if(r.message) {
+					var val = frm.add_child('finger_print_details');
+					val['id'] = r.message[0];
+					val['template'] = r.message[1];
+				}
+				refresh_field('finger_print_details');
+				frappe.msgprint(__("Successfully Enrolled"));
+				frm.save();
+			}
+		});
+		d.hide();
+	}
+	});
+	d.show();
     },
-    delete_employee: function(frm){
-        frappe.call({
-			method: "essdee_attendance.essdee_attendance.doctype.essdee_attendance_settings.essdee_attendance_settings.delete_employee",
-			freeze: true,
-            args: {id: frm.doc.attendance_device_id, work_location: frm.doc.work_location},
-        }
-        )
-    },
-    enroll_user: function(frm){
-        frappe.call({
-			method: "essdee_attendance.essdee_attendance.doctype.essdee_attendance_settings.essdee_attendance_settings.enroll_user",
-			freeze: true,
-            args: {id: frm.doc.attendance_device_id, work_location: frm.doc.work_location},
-    }
-        )
-    }
-
+    sync_now: function(frm){
+		frappe.call({
+			method: "essdee_attendance.essdee_attendance.doctype.essdee_attendance_settings.essdee_attendance_settings.sync_now",
+			args: {employee:frm.doc},
+			freeze: true
+		})
+	}
 });
