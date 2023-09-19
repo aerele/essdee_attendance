177bb68fccca3f24097ec2739c4f504d8e61b5be
Dwaraka Vikraman <dwarakavikraman@gmail.com>
2023-09-19 17:35:18 +0530

    Updated Weekly Employee Report

--- essdee_attendance/essdee_attendance/report/weekly_employee_report/weekly_employee_report.js
+++ essdee_attendance/essdee_attendance/report/weekly_employee_report/weekly_employee_report.js
@@ -2,17 +2,43 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+let cur_cell_height = 1;
+function get_cell_height(rows) {
+	let cellHeights = [33, 33, 50, 66, 83]
+	if (rows < 0 ) {
+		return 33;
+	} else if (rows >= 0 && rows < cellHeights.length) {
+		return cellHeights[rows]
+	} else {
+		return 83;
+	}
+}
+
+
+function set_cell_height() {
+	if (!frappe.query_report.datatable) return;
+	let rows = get_rows();
+	if (rows != cur_cell_height) {
+		frappe.query_report.datatable.options.cellHeight = get_cell_height(rows);
+		// frappe.query_report.datatable.refresh(frappe.query_report.data, frappe.query_report.columns)
+	}
+	cur_cell_height = rows;
+}
+
+function get_rows() {
+	if (frappe.query_report.get_filter_value('summarized_view')) return 1;
+	let rows = 0;
+	let x = ['show_in_out', 'show_time_logs', 'show_hours', 'show_shift'];
+	for (let i = 0; i < x.length; i++) {
+		if (frappe.query_report.get_filter_value(x[i])) {
+			rows += 1;
+		}
+	}
+	return rows || 1;
+}
+
 frappe.query_reports["Weekly Employee Report"] = {
 	"filters": [
-		{
-			"fieldname":"company",
-			"label": __("Company"),
-			"fieldtype": "Link",
-			"options": "Company",
-			"reqd": 1,
-			"default": frappe.defaults.get_user_default("Company"),
-			"read_only": 1
-		},
 		{
 			"fieldname":"from_date",
 			"label": __("From Date"),
@@ -31,20 +57,80 @@ frappe.query_reports["Weekly Employee Report"] = {
 			"fieldname":"employee",
 			"label": __("Employee"),
 			"fieldtype": "Link",
-			"options": "Employee"
+			"options": "Employee",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value('company');
+				return {
+					filters: {
+						'company': company
+					}
+				};
+			}
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
 		},
 		{
 			"fieldname":"summarized_view",
 			"label": __("Summarized View"),
 			"fieldtype": "Check",
-			"Default": 0,
+			"default": 0,
+			on_change: () => {
+				set_cell_height();
+				frappe.query_report.refresh();
+			}
+		},
+		{
+			"fieldname":"show_in_out",
+			"label": __("Show In Out"),
+			"fieldtype": "Check",
+			"default": 1,
+			on_change: () => {
+				set_cell_height();
+				frappe.query_report.refresh();
+			}
 		},
 		{
 			"fieldname":"show_time_logs",
 			"label": __("Show Time Logs"),
 			"fieldtype": "Check",
-			"Default": 0,
-		}
+			"default": 0,
+			on_change: () => {
+				set_cell_height();
+				frappe.query_report.refresh();
+			}
+		},
+		{
+			"fieldname":"show_hours",
+			"label": __("Show Hours"),
+			"fieldtype": "Check",
+			"default": 1,
+			on_change: () => {
+				set_cell_height();
+				frappe.query_report.refresh();
+			}
+		},
+		{
+			"fieldname":"show_shift",
+			"label": __("Show Shift"),
+			"fieldtype": "Check",
+			"default": 1,
+			on_change: () => {
+				set_cell_height();
+				frappe.query_report.refresh();
+			}
+		},
+	],
 
-	]
-};
+	get_datatable_options(options) {
+		let rows = get_rows();
+		return Object.assign(options, {
+			cellHeight: get_cell_height(rows),
+		})
+	},
+}
--- essdee_attendance/essdee_attendance/report/weekly_employee_report/weekly_employee_report.py
+++ essdee_attendance/essdee_attendance/report/weekly_employee_report/weekly_employee_report.py
@@ -1,94 +1,189 @@
 # Copyright (c) 2013, Aerele and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import add_days, getdate, cstr
+from frappe.utils import getdate, add_days, cstr, get_datetime
 
-day_abbr = [
-	"Mon",
-	"Tue",
-	"Wed",
-	"Thu",
-	"Fri",
-	"Sat",
-	"Sun"
-]
+day_abbr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 def execute(filters=None):
+	filters = frappe._dict(filters or {})
 	columns, data = [], []
-	if filters:
-		columns = get_columns(filters, columns)
-		data = get_data(filters, data)
+	if not filters.from_date or not filters.to_date:
+		frappe.throw("Set Date Range")
+	
+	columns = get_columns(filters)
+	data = get_data(filters)
 	return columns, data
-
-def get_columns(filters, columns):
-	columns += [
-		_("Employee") + ":Link/Employee:120", _("Employee Name") + ":Data/:120"
+	
+def get_columns(filters):
+	columns = [
+		{
+			"label": _("Employee"),
+			"fieldname": "employee",
+			"fieldtype": "Link",
+			"options": "Employee",
+			"width": 115,
+		},
+		{"label": _("Employee Name"), "fieldname": "employee_name", "fieldtype": "Data", "width": 120},
 	]
-	week_date_list = []
-	from_date = getdate(filters['from_date'])
-	to_date = getdate(filters['to_date'])
-	date_range = (to_date - from_date).days
-	for idx in range(date_range+1):
-		day_name = day_abbr[from_date.weekday()]
-		week_date_list.append(cstr(from_date.strftime("%d-%m-%Y"))+ " " +day_name +"::120")
-		from_date = add_days(from_date, 1)
-	if not filters.summarized_view or filters.show_time_logs:
-		columns += week_date_list
-	if filters.summarized_view:
-		columns += [
-			_("Total Shift") + ":Float:100",
-			_("Rate") + ":Currency:70",
-			_("Amount") + ":Currency:150"
-		]
-	if filters.summarized_view and filters.show_time_logs:
-		columns = []
+	if not filters.summarized_view:
+		columns.extend(get_columns_for_days(filters))
+	else:
+		columns.extend([
+			{"label": _("Shift"), "fieldname": "shift", "fieldtype": "Data", "width": 65},
+			{"label": _("No. of Shifts"), "fieldname": "shift_count", "fieldtype": "Float", "width": 120},
+			{"label": _("Rate"), "fieldname": "rate", "fieldtype": "Float", "width": 120},
+			{"label": _("Amount"), "fieldname": "amount", "fieldtype": "Float", "width": 120},
+		])
 	return columns
 
-def get_data(filters, data):
-	if filters.summarized_view and filters.show_time_logs:
-		return []
-	if 'employee' in filters:
-		employee_details = frappe.db.get_values('Employee',filters['employee'], \
-			fieldname=['name', 'employee_name','sd_shift_rate'], as_dict=True)
-	else:
-		employee_details = frappe.get_all("Employee",fields=['name', 'employee_name','sd_shift_rate'])
-	for employee_detail in employee_details:
-		if frappe.db.exists("Attendance", {'employee':employee_detail['name'], 'status':'Present', 'docstatus': 1}):
-			data.append(get_employee_specific_data(employee_detail, filters))
+def get_columns_for_days(filters):
+	days = []
+	date = getdate(filters.from_date)
+	to_date = getdate(filters.to_date)
+	
+	while date <= to_date:
+		weekday = day_abbr[date.weekday()]
+		label = "{} {}".format(cstr(date.day), weekday)
+		days.append({"label": label, "fieldtype": "Data", "fieldname": date.isoformat(), "width": 145})
+		date = add_days(date, 1)
+	return days
+
+def get_data(filters):
+	data = []
+	employees = get_employees(filters)
+	attendance_records = get_attendance_map(filters)
+	attendance_records = get_checkin_map(attendance_records, filters)
+	for employee in employees:
+		d = {
+			'employee': employee.name,
+			'employee_name': employee.employee_name
+		}
+		if not filters.summarized_view:
+			get_employee_detail(d, filters, employee, attendance_records.get(employee.name))
+		else:
+			get_summarized_detail(d, filters, employee, attendance_records.get(employee.name))
+		data.append(d)
 	return data
 
+def get_summarized_detail(data, filters, employee, attendance_records):
+	if attendance_records:
+		total_shift = 0
+		for date, value in attendance_records.items():
+			total_shift += (value.get('sd_no_of_shifts') or 0)
+		data.update({
+			'shift_count': total_shift,
+			'rate': employee.sd_shift_rate,
+			'amount': total_shift * (employee.sd_shift_rate or 0)
+		})
 
-def get_employee_specific_data(employee_detail, filters):
-	if filters.summarized_view:
-		shifts = frappe.get_list('Attendance',{'attendance_date':('between',[filters['from_date'],filters['to_date']]), \
-			'employee':employee_detail['name'], 'status':'Present', 'docstatus': 1}, ['sd_no_of_shifts'], as_list = True)
-		total_shift = sum([float(shift[0]) for shift in shifts if shift[0]])
-		return {
-			'employee': employee_detail['name'],
-			'employee_name': employee_detail['employee_name'],
-			'total_shift': total_shift,
-			'rate': employee_detail['sd_shift_rate'],
-			'amount': total_shift * employee_detail['sd_shift_rate']}
-	else:
-		from_date = getdate(filters['from_date'])
-		to_date = getdate(filters['to_date'])
-		date_range = (to_date - from_date).days
-		data = [employee_detail['name'],employee_detail['employee_name']]
-		for idx in range(date_range+1):
+def get_employee_detail(data, filters, employee, attendance_records):
+	if attendance_records:
+		for date, value in attendance_records.items():
+			detail = []
+			if filters.show_in_out:
+				in_time = None
+				out_time = None
+				if value.get('in_time'):
+					in_time = value['in_time'].strftime("%H:%M")
+				if value.get('out_time'):
+					out_time = value['out_time'].strftime("%H:%M")
+				detail.append("{} - {}".format(in_time, out_time))
 			if filters.show_time_logs:
-				time_log = ''
-				attendance = frappe.db.get_value('Attendance',{'attendance_date': from_date, \
-					'employee':employee_detail['name'], 'status':'Present', 'docstatus': 1})
-				if attendance:
-					employee_checkin_list = frappe.get_list('Employee Checkin',{'attendance':attendance},'time')
-					time_log = ', '.join([data['time'].strftime("%H:%M") for data in employee_checkin_list])
-				data.append(time_log)
-			else:
-				shift = frappe.db.get_value('Attendance',{'attendance_date': from_date, \
-					'employee':employee_detail['name'], 'status':'Present', 'docstatus': 1}, ['sd_no_of_shifts'])
-				data.append(shift if shift else 0)
-			from_date = add_days(from_date, 1)
-		return data
+				detail.append(", ".join(value.get('checkin') or []))
+			if filters.show_hours:
+				detail.append("{} - {}".format(value.get('status') or '', value.get('working_hours') or 0))
+			if filters.show_shift:
+				detail.append(str(value.get('sd_no_of_shifts') or 0))
+			data[date] = "<br>".join(detail)
+
+def get_employees(filters):
+	Employee = frappe.qb.DocType("Employee")
+	
+	query = frappe.qb.from_(Employee).select(Employee.name, Employee.employee_name, Employee.sd_shift_rate)
+	if filters.employee:
+		query = query.where(Employee.name == filters.employee)
+	return query.run(as_dict = 1)
+
+
+def get_attendance_map(filters):
+	attendance_list = get_attendance_records(filters)
+	attendance_map = {}
+
+	for d in attendance_list:
+
+		attendance_map.setdefault(d.employee, {})
+		attendance_map[d.employee][d.attendance_date.isoformat()] = {
+			'employee_name': d.employee_name,
+			'status': d.status,
+			'shift': d.shift,
+			'sd_no_of_shifts': d.sd_no_of_shifts,
+			'in_time': d.in_time,
+			'out_time': d.out_time,
+			'working_hours': d.working_hours,
+		}
+
+	return attendance_map
+
+def get_attendance_records(filters):
+	Attendance = frappe.qb.DocType("Attendance")
+	
+	query = (
+		frappe.qb.from_(Attendance)
+		.select(
+			Attendance.employee,
+			Attendance.attendance_date,
+			Attendance.status,
+			Attendance.shift,
+			Attendance.sd_no_of_shifts,
+			Attendance.in_time,
+			Attendance.out_time,
+			Attendance.working_hours,
+		).where(
+			(Attendance.docstatus == 1)
+			& (Attendance.company == filters.company)
+			& (Attendance.attendance_date >= filters.from_date)
+			& (Attendance.attendance_date <= filters.to_date)
+		)
+	)
+	
+	if filters.employee:
+		query = query.where(Attendance.employee == filters.employee)
+	query = query.orderby(Attendance.employee, Attendance.attendance_date)
+
+	return query.run(as_dict=1)
+
+def get_checkin_map(attendance_records, filters):
+	if not filters.show_time_logs:
+		return attendance_records
+	checkin_list = get_check_in_records(filters)
+
+	for d in checkin_list:
+		date = getdate(d.time).isoformat()
+		attendance_records.setdefault(d.employee, {}).setdefault(date, {}).setdefault('checkin', [])
+		attendance_records[d.employee][date]['checkin'].append(d.time.strftime("%H:%M"))
+
+	return attendance_records
+
+def get_check_in_records(filters):
+	EmployeeCheckin = frappe.qb.DocType("Employee Checkin")
+	
+	query = (
+		frappe.qb.from_(EmployeeCheckin)
+		.select(
+			EmployeeCheckin.employee,
+			EmployeeCheckin.time,
+		).where(
+			(EmployeeCheckin.attendance.notnull())
+			& (EmployeeCheckin.time >= filters.from_date)
+			& (EmployeeCheckin.time <= filters.to_date)
+		)
+	)
+	
+	if filters.employee:
+		query = query.where(EmployeeCheckin.employee == filters.employee)
+	query = query.orderby(EmployeeCheckin.employee, EmployeeCheckin.time)
+
+	return query.run(as_dict=1)
