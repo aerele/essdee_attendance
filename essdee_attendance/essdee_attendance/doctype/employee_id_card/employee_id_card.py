58b6d5976b4d2c4c780725affb6cb7e7e9c7e5fe
MohammedAnassBE <mohammedanasman123@gmail.com>
2024-04-18 19:13:02 +0530

    ID card Edit

--- essdee_attendance/essdee_attendance/doctype/employee_id_card/employee_id_card.py
+++ essdee_attendance/essdee_attendance/doctype/employee_id_card/employee_id_card.py
@@ -7,3 +7,9 @@ from frappe.model.document import Document
 
 class EmployeeIDCard(Document):
 	pass
+
+# def get_qr_code_base64_str(content, scale=1):
+# 	from pyqrcode import create as qrcreate
+# 	qr = qrcreate(content)
+# 	qr_str = qr.png_as_base64_str(scale=scale)
+# 	return qr_str
