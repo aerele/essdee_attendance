e990f231805415f0d0d97b2963e3a44d73aa730c
Dwaraka Vikraman <dwarakavikraman@gmail.com>
2024-08-31 17:02:08 +0530

    removed gitignore files

--- essdee_attendance/public/frontend/assets/status-tap-DlwxxGXU.js
+++
@@ -1,4 +0,0 @@
-var d=(o,r,n)=>new Promise((s,t)=>{var i=e=>{try{a(n.next(e))}catch(c){t(c)}},l=e=>{try{a(n.throw(e))}catch(c){t(c)}},a=e=>e.done?s(e.value):Promise.resolve(e.value).then(i,l);a((n=n.apply(o,r)).next())});import{t as w,v as m,w as p,x as h,y}from"./index-BxNz1Obz.js";import"./frappe-ui-DZDZpt4w.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const u=()=>{const o=window;o.addEventListener("statusTap",()=>{w(()=>{const r=o.innerWidth,n=o.innerHeight,s=document.elementFromPoint(r/2,n/2);if(!s)return;const t=m(s);t&&new Promise(i=>p(t,i)).then(()=>{h(()=>d(void 0,null,function*(){t.style.setProperty("--overflow","hidden"),yield y(t,300),t.style.removeProperty("--overflow")}))})})})};export{u as startStatusTap};
-//# sourceMappingURL=status-tap-DlwxxGXU.js.map
